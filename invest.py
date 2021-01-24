738823f4e48217494d02f0a9f2deac75a98aa2a0
ernestchng <ernestchngyongcheng97@gmail.com>
2021-01-24 18:47:06 +0800

    Updated requirements.txt

--- invest.py
+++ invest.py
@@ -1,8 +1,9 @@
 ##NUS INVEST VOLATILITY MODELLING
 
-import talib
 import pickle
+
 import pandas as pd
+import talib
 
 """
 Output of Script: DB structure with ["pair1 open", "pair1 high"..."pair1 feature1", "pair1 feature2"..."pairN featureN"]
@@ -23,15 +24,16 @@ OBV (Volume)
 
 instruments = [
     "EUR_USD",
-    "AUD_USD", 
+    "AUD_USD",
     "GBP_USD",
-    "USD_CAD", 
-    "NZD_USD", 
-    "USD_CHF", 
-    "USD_JPY", 
-    "USD_NOK", 
+    "USD_CAD",
+    "NZD_USD",
+    "USD_CHF",
+    "USD_JPY",
+    "USD_NOK",
     "USD_SEK"
 ]
+
 
 def get_fx_db():
     ohlcv = pd.read_excel("./database/dwx_ohlcv.xlsx").set_index("date")
@@ -49,16 +51,18 @@ def get_fx_db():
 
 # exit()
 
-file = open("fx_db.obj",'rb')
+file = open("fx_db.obj", 'rb')
 fx_db = pickle.load(file)
 file.close()
 
 import datetime
 import pandas_datareader as pdr
 
+
 def format_date(series):
     ymd = list(map(lambda x: int(x), str(series).split(" ")[0].split("-")))
     return datetime.date(ymd[0], ymd[1], ymd[2])
+
 
 vix_index = pdr.get_data_yahoo("^VIX", fx_db.index[1], fx_db.index[-1])
 vix_index.columns = list(map(lambda x: "{} {}".format("VIX", x), list(vix_index)))
@@ -75,23 +79,22 @@ print(df)
 
 for inst in instruments:
     """ATR"""
-    df["{} {}".format(inst, "ATR")] = talib.ATR(df["{} high".format(inst)], df["{} low".format(inst)], df["{} close".format(inst)], timeperiod = 14)
-    
+    df["{} {}".format(inst, "ATR")] = talib.ATR(df["{} high".format(inst)], df["{} low".format(inst)], df["{} close".format(inst)], timeperiod=14)
+
     """OBV"""
     df["{} {}".format(inst, "OBV")] = talib.OBV(df['{} close'.format(inst)], df['{} volume'.format(inst)])
 
     """RSI"""
-    df["{} {}".format(inst, "RSI")] = talib.RSI(df["{} close".format(inst)], timeperiod = 14)
+    df["{} {}".format(inst, "RSI")] = talib.RSI(df["{} close".format(inst)], timeperiod=14)
 
     """BBANDS"""
     bband = pd.DataFrame()
     bband["MA"] = df["{} close".format(inst)].rolling(14).mean()
-    bband["stdev"] = df["{} close".format(inst)].rolling(14).std() 
+    bband["stdev"] = df["{} close".format(inst)].rolling(14).std()
     bband["BB_up"] = bband["MA"] + 2 * bband["stdev"]
     bband["BB_dn"] = bband["MA"] - 2 * bband["stdev"]
     df["{} BB_up".format(inst)] = bband["BB_up"]
     df["{} BB_dn".format(inst)] = bband["BB_dn"]
 
-    
 print(df)
 df.to_excel("df.xlsx")
